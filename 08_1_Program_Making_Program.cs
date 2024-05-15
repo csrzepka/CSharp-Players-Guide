e515ab1cd6798dbdb828d890a1c60242e9081c77
Christopher Rzepka <christopher.rzepka@sympatico.ca>
2024-05-15 17:14:46 -0400

    L8 E11 challenges

---
+++ 08_1_Program_Making_Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("What units do you want to use?");
+string units = Console.ReadLine();
+
+Console.WriteLine("What C# type do you want to use?");
+string type = Console.ReadLine();
+
+string program = $$"""
+    Console.WriteLine("Enter the width (in {{units}}) of the triangle: ");
+    {{type}} width = {{type}}.Parse(Console.ReadLine());
+    Console.WriteLine("Enter the height (in {{units}}) of the triangle: ");
+    {{type}} height = {{type}}.Parse(Console.ReadLine());
+    {{type}} result = width * height / 2;
+    Console.WriteLine($"{result} square {{units}}");
+    """;
+
+Console.Write(program);
+
+// Console.WriteLine("Enter the width (in cm) of the triangle: ");
+// int width = int.Parse(Console.ReadLine());
+// Console.WriteLine("Enter the height (in cm) of the triangle: ");
+// int height = int.Parse(Console.ReadLine());
+// int result = width * height / 2;
+// Console.WriteLine($"{result} square cm");
