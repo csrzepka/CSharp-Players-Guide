a28f79d9a1d05d6b898e68cf9f3a5d47fba1c456
Christopher Rzepka <christopher.rzepka@sympatico.ca>
2024-05-16 16:28:07 -0400

    L12 challenges

---
+++ 12_TheLawsOfFreach.cs
@@ -0,0 +1,54 @@
+﻿/*
+Challenge: The Laws of Freach [50 XP]
+
+The town of Freach had an awful looping disaster. The lead investigator found
+that it was a faulty ++ operator in an old for loop, but the town council has
+chosen to ban all loops but the foreach loop. Yet Freach uses the code
+presented earlier in this level to compute the minimum and average value in an
+int array. They have hired you to rework their existing for-based code to use
+foreach loops instead.
+
+Objectives:
+- Start with the code for computing an array's minimum and average values in
+  the section Some Examples with Arrays, starting on page 94.
+- Modify the code to use foreach loops instead of for loops.
+*/
+
+// Min Value Example:
+// int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+//
+// int currentSmallest = int.MaxValue; // Start higher than anything in the array.
+// for (int index = 0; index < array.Length; index++)
+// {
+// 	if (array[index] < currentSmallest)
+// 		currentSmallest = array[index];
+// }
+//
+// Console.WriteLine(currentSmallest);
+
+// Average Value Example:
+// int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+//
+// int total = 0;
+// for (int index = 0; index < array.Length; index++)
+// 	total += array[index];
+//
+// float average = (float)total / array.Length;
+// Console.WriteLine(average);
+
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+// Min Value
+int currentSmallest = int.MaxValue; // Start with high value
+foreach (int x in array)
+    if (x < currentSmallest) currentSmallest = x;
+
+Console.WriteLine(currentSmallest);
+
+// Average Value
+int total = 0;
+foreach (int x in array)
+    total += x;
+
+float average = (float)total / array.Length;
+Console.WriteLine(average);
---
+++ 12_TheReplicatorOfDTo.cs
@@ -0,0 +1,35 @@
+﻿/*
+Challenge: The Replicator of D'To [100 XP]
+
+While searching an abandoned storage building containing strange artifacts, you
+uncover the ancient Replicator of D'To. This can replicate the contents of any
+int array into another array. But it appears broken and needs a Programmer to
+reforge the magic that allows it to replicate once again.
+
+Objectives:
+- Make a program that creates an array of length 5.
+- Ask the user for five numbers and put them in the array.
+- Make a second array of length 5.
+- Use a loop to copy the values out of the original array and into the new one.
+- Display the contents of both arrays one at a time to illustrate that the
+  Replicator of D'To works again.
+*/
+
+int[] array = new int[5];
+
+Console.WriteLine("Enter 5 values to fill an array:");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{i}: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int[] copyArray = new int[5];
+
+Console.WriteLine("Copying the array values to a second array...");
+for (int i = 0; i < copyArray.Length; i++)
+    copyArray[i] = array[i];
+
+Console.WriteLine("Values of both arrays:");
+for (int i = 0; i < 5; i++)
+    Console.WriteLine($"{i}: {array[i]}, {copyArray[i]}");
