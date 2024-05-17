9b504be599a6378376c17fc6151769abd42e4c6d
Christopher Rzepka <christopher.rzepka@sympatico.ca>
2024-05-16 21:07:30 -0400

    L13 challenges

---
+++ 13_Countdown.cs
@@ -0,0 +1,40 @@
+﻿/*
+Challenge: Countdown [100 XP]
+Note: This challenge requires reading The Basics of Recursion side quest to 
+attempt.
+
+The Council of Freach has summoned you. New writing has appeared on the Tomb of
+Algol the Wise, the last True Programmer to wander this land. The writing
+strikes fear and awe into the hearts of the loop-loving people of Freach:
+"The next True Programmer shall be able to write any looping code with a method
+call instead." The Senior Counselor, scared of a world without loops, asks you
+to put your skills to the test and rewrite the following code, which counts
+down from 10 to 1, with no loops:
+```
+for (int x = 10; x > 0; x--)
+	Console.WriteLine(x);
+```
+As you consider the words on the Tomb of Algol the Wise, you begin to think it
+might be correct that you write this code using recursion instead of a loop.
+
+Objectives:
+- Write code that counts down from 10 to 1 using a recursive method.
+- Hint: Remember that you must have a base case that ends the recursion and
+  that every time you call the method recursively, you must be getting closer
+  and closer to that base case.
+*/
+
+CountDown(10);
+
+/// <summary>
+/// Counts down from the given number, to 1.
+/// </summary>
+void CountDown(int numberToCountDownFrom)
+{
+    // Base Case: return when numberToCountDownFrom is 0
+    if (numberToCountDownFrom == 0)
+        return;
+
+    Console.WriteLine(numberToCountDownFrom);
+    CountDown(numberToCountDownFrom - 1);
+}
---
+++ 13_TakingANumber.cs
@@ -0,0 +1,63 @@
+﻿/*
+Challenge: Taking a Number [100 XP]
+
+Many previous tasks have required getting a number from a user. To save time
+writing this code repeatedly, you have decided to make a method to do this
+common task.
+
+Objectives:
+- Make a method with the signature int AskForNumber(string text). Display the
+  text parameter in the console window, get a response from the user, convert
+  it to an int, and return it. This might look like this: 
+  int result = AskForNumber("What is the airspeed velocity of an unladen 
+  swallow?");
+- Make a method with the signature: 
+  int AskForNumberInRange(string text, int min, int max). 
+  Only return if the entered number is between the min and max values. 
+  Otherwise, ask again.
+- Place these methods in at least one of your previous programs to improve it.
+*/
+
+/// <summary>
+/// Prompts user with <string>text to enter a number and returns number.
+/// </summary>
+int AskForNumber(string text)
+{
+    Console.Write(text + " ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+/// <summary>
+/// Prompt user to enter a number in range min to max, using message text.
+/// Will ask user again until number is within range.
+/// </summary>
+int AskForNumberInRange(string text, int min, int max)
+{
+    int number;
+
+    do
+    {
+        Console.Write(text + " ");
+        number = Convert.ToInt32(Console.ReadLine());
+    } 
+    while (number < min || number > max);
+
+    return number;
+}
+
+// Repeating Challenge: The Prototype (Level 11) with new methods
+int number = AskForNumberInRange("User 1, enter a number between 0 and 100:", 0, 100);
+Console.Clear();
+Console.WriteLine("User 2, guess the number.");
+
+while (true)
+{
+    int guess = AskForNumber("What is your next guess?");
+
+    if (guess > number) Console.WriteLine($"{guess} is too high.");
+    else if (guess < number) Console.WriteLine($"{guess} is too low.");
+    else break;
+}
+
+Console.WriteLine("You guessed the number!");
